c6b165358e89475e7f5c8da03e816327dae032db
Egor <OneTells@ya.ru>
2024-05-05 16:59:57 +0500

    Исправлена работа с with

--- utils/modules/database/methods/transaction.py
+++ utils/modules/database/methods/transaction.py
@@ -1,5 +1,3 @@
-from typing import Self
-
 from asyncpg import transaction, Connection
 from asyncpg.pool import PoolAcquireContext
 
@@ -12,14 +10,14 @@ class Transaction:
         self.__pool_context: PoolAcquireContext | None = None
         self.__transaction: transaction.Transaction | None = None
 
-    async def __aenter__(self) -> Self:
+    async def __aenter__(self) -> Connection:
         self.__pool_context = DatabasePool.get_pool().acquire()
         await self.__pool_context.__aenter__()
 
         self.__transaction = self.__pool_context.connection.transaction()
         await self.__transaction.__aenter__()
 
-        return self
+        return self.__pool_context.connection
 
     async def __aexit__(self, *exc) -> None:
         await self.__transaction.__aexit__(*exc)
@@ -27,7 +25,3 @@ class Transaction:
 
         await self.__pool_context.__aexit__(*exc)
         self.__pool_context = None
-
-    @property
-    def connection(self) -> Connection:
-        return self.__pool_context.connection
