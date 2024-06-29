7e9f4088d84ced0fbbdc61690cba853367329461
Egor <OneTells@ya.ru>
2024-06-30 01:18:24 +0500

    Добавление модуля кэширования

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@ from loguru import logger as _logger
 from .modules import *
 from .modules.logger.handlers.telegram import Telegram
 
-__version__ = "1.5.9"
+__version__ = "1.5.10"
 
 _logger.disable('utils')
 
